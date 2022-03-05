f472390e85cda1c3593cdc73d7a9bf58f1b6b2b5
bung87 <crc32@qq.com>
2022-03-05 12:54:35 +0800

    clean hmisc

--- src/slimpkg/hmisc/hcommon_converters.nim
+++
@@ -1,8 +0,0 @@
-import types/[seq2d, hprimitives]
-
-# REFACTOR remove
-converter toSeq2D*[T](s: seq[seq[T]]): Seq2d[T] =
-  makeSeq2D(s)
-
-converter toStrBlock*(s: seq[string]): StrBlock =
-  StrBlock(s)
